542ec220229f041f2e7e1e12721b22481fb562a7
aman0303 <39325082+aman0303@users.noreply.github.com>
2022-09-14 12:04:00 +0530

    Update updateConfigAndAppluDeployment.sh

--- updateConfigAndAppluDeployment.sh
+++ updateConfigAndAppluDeployment.sh
@@ -12,24 +12,24 @@ updatedAppSettingsFile="./appsettings.prod.json";
 
 DatabaseServiceEndpoint="http://localhost/8080";
 
-sed -r -E -e "s/##DATABASE_ID##/$DatabaseId/g" \
-          -e "s/##DATABASE_SERVICE_ENDPOINT##/$DatabaseServiceEndpoint/g" \
-          -e "s/##DATABASE_ACCOUNT_URI##/$DatabaseAccountUri/g" \
-          -e "s/##DATABASE_DATABASE_ID##/$DatabaseDatabaseId/g" \
-          -e "s/##BLOB_STORAGE_PROVIDER_ID##/$BlobStorageProviderId/g" \
-          -e "s/##BLOB_STORAGE_SUBSCRIPTION##/$BlobStorageSubscription/g" \
-          -e "s/##BLOB_STORAGE_RESOURCE_GROUP##/$BlobStorageResourceGroup/g" \
-          -e "s/##BLOB_STORAGE_ACCOUNT_NAME##/$BlobStorageAccountName/g" \
-          -e "s/##BLOB_STORAGE_ACCOUNT_URL##/$BlobStorageAcountUrl/g" \
-          -e "s/##SYNAPSE_WORKSPACE_NAME##/$SynapseWorkspaceName/g" \
-          -e "s/##JOB_MANAGEMENT_QUEUE_CONNECTION_STRING##/$JobManagementQueueConnectionString/g" \
-          -e "s/##JOB_MANAGEMENT_SERVICE_ENDPOINT##/$JobManagementServiceEndpoint/g" \
-          -e "s/##JOB_MANAGEMENT_ACCOUNT_URI##/$JobManagementAccountUri/g" \
-          -e "s/##JOB_MANAGEMENT_DATABASE_NAME##/$JobManagementDatabaseName/g" \
-          -e "s/##JOB_MANAGEMENT_COLLECTION_NAME##/$JobManagementCollectionName/g" \
-          -e "s/##JOB_MANAGEMENT_QUEUE_NAME_PREFIX##/$JobManagementQueueNamePrefix/g" \
-          -e "s/##APP_INSIGHTS_INSTRUMENTATION_KEY##/$AppInsightsInstrumentationKey/g" \
-          -e "s/##HOSTED_ENV_MSI_CLIENT_ID##/$HostedEnvMsiVlientId/g" $appSettingsFile > $updatedAppSettingsFile;
+sed -r -E -e "s%##DATABASE_ID##%$DatabaseId%g" \
+          -e "s%##DATABASE_SERVICE_ENDPOINT##%$DatabaseServiceEndpoint%g" \
+          -e "s%##DATABASE_ACCOUNT_URI##%$DatabaseAccountUri%g" \
+          -e "s%##DATABASE_DATABASE_ID##%$DatabaseDatabaseId%g" \
+          -e "s%##BLOB_STORAGE_PROVIDER_ID##%$BlobStorageProviderId%g" \
+          -e "s%##BLOB_STORAGE_SUBSCRIPTION##%$BlobStorageSubscription%g" \
+          -e "s%##BLOB_STORAGE_RESOURCE_GROUP##%$BlobStorageResourceGroup%g" \
+          -e "s%##BLOB_STORAGE_ACCOUNT_NAME##%$BlobStorageAccountName%g" \
+          -e "s%##BLOB_STORAGE_ACCOUNT_URL##%$BlobStorageAcountUrl%g" \
+          -e "s%##SYNAPSE_WORKSPACE_NAME##%$SynapseWorkspaceName%g" \
+          -e "s%##JOB_MANAGEMENT_QUEUE_CONNECTION_STRING##%$JobManagementQueueConnectionString%g" \
+          -e "s%##JOB_MANAGEMENT_SERVICE_ENDPOINT##%$JobManagementServiceEndpoint%g" \
+          -e "s%##JOB_MANAGEMENT_ACCOUNT_URI##%$JobManagementAccountUri%g" \
+          -e "s%##JOB_MANAGEMENT_DATABASE_NAME##%$JobManagementDatabaseName%g" \
+          -e "s%##JOB_MANAGEMENT_COLLECTION_NAME##%$JobManagementCollectionName%g" \
+          -e "s%##JOB_MANAGEMENT_QUEUE_NAME_PREFIX##%$JobManagementQueueNamePrefix%g" \
+          -e "s%##APP_INSIGHTS_INSTRUMENTATION_KEY##%$AppInsightsInstrumentationKey%g" \
+          -e "s%##HOSTED_ENV_MSI_CLIENT_ID##%$HostedEnvMsiVlientId%g" $appSettingsFile > $updatedAppSettingsFile;
 
 UUID=$(cat /proc/sys/kernel/random/uuid);
 AppSettingsConfigmap="mds-app-settings-configmap-$UUID";
@@ -39,8 +39,8 @@ echo "--------------------------------------------------------------------------
 cat $appSettingsFile;
 echo "---------------------------------------------------------------------------------------------";
 
-sed -r -E -e "s/##MDS_SERVICE_IMAGE_NAME##/$IdsServiceImageName/g" \
-          -e "s/##MDS_APP_SETTINGS_CONFIGMAP##/$AppSettingsConfigmap/g" $k8DeploymentFile > $updatedK8DeploymentFile;
+sed -r -E -e "s%##MDS_SERVICE_IMAGE_NAME##%$IdsServiceImageName%g" \
+          -e "s%##MDS_APP_SETTINGS_CONFIGMAP##%$AppSettingsConfigmap%g" $k8DeploymentFile > $updatedK8DeploymentFile;
 
 cat $updatedK8DeploymentFile;
 echo "---------------------------------------------------------------------------------------------";
